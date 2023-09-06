77d506dec827ecf0fa34f77f8b7a3d8ee5fc96d9
stellick.m <stellick.m@stu.janesville.k12.wi.us>
2023-09-06 11:36:32 -0500

    9/6 2

---
+++ src/Q1/Prog54b.java
@@ -0,0 +1,15 @@
+package Q1;
+
+public class Prog54b {
+    public static void main(String [] args) {
+        int num1 = 475;
+        int num2 = 821;
+        int num3 = 369;
+        int num4 = 562;
+
+        int sum = num1 +  num2 + num3 + num4;
+        System.out.println("The sum of the four numbers is " + sum);
+        double avg = sum / 4;
+        System.out.println ("The average of the four numbers is " + avg);
+    }
+}
