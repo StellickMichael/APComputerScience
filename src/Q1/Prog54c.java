6b7601eb665aab49aa14b4f23c23c3a1e91c47b0
stellick.m <stellick.m@stu.janesville.k12.wi.us>
2023-09-07 11:15:30 -0500

    9/7

--- src/Q1/Prog54c.java
+++ src/Q1/Prog54c.java
@@ -3,6 +3,8 @@ package Q1;
 public class Prog54c {
     public static void main(String[] args) {
         double constant = 3.14159;
+        double radius = 3.712;
+        double circumference = 2 * radius;
 
     }
 }
