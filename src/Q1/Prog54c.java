215a905c1db842f94264d8c720f2f0bee35bc081
stellick.m <stellick.m@stu.janesville.k12.wi.us>
2023-09-08 11:20:30 -0500

    9/8

--- src/Q1/Prog54c.java
+++ src/Q1/Prog54c.java
@@ -2,9 +2,11 @@ package Q1;
 
 public class Prog54c {
     public static void main(String[] args) {
-        double constant = 3.14159;
+        double cons = 3.14159;
         double radius = 3.712;
-        double circumference = 2 * radius;
-        double area = constant * radius%2;
+        double cir = 2 * radius;
+        double area = cons * radius%2;
+
+        System.out.printf("The Radius of the Circle is $%.3\n" ,radius);
     }
 }
