f6d500a5bc024b0a7d6f22009ed4444f473e5553
stellick.m <stellick.m@stu.janesville.k12.wi.us>
2023-09-12 11:05:21 -0500

    9/12

---
+++ src/Q1/GradeCalc.java
@@ -0,0 +1,23 @@
+package Q1;
+
+import java.util.*;
+
+public class GradeCalc {
+    public static void main(String[] args) {
+        // input section
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your grade as a percent: ");
+        double gp = input.nextDouble();
+        // calculation section
+
+        String grade = "";
+        if (gp >= 90) grade = "A";
+        else if (gp >= 80) grade = "B";
+        else if (gp >= 70) grade = "C";
+        else if (gp >= 60) grade = "D";
+        else grade = "F";
+
+        // output section
+        System.out.println("Your grade is a(n) " + grade);
+    }
+}
